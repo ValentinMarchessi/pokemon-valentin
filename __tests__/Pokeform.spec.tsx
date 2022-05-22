7f2fb3c858bde44c89fe8176bce38127ee8750ff
Valentin Marchessi <valentin.marchessi@devsu.com>
2022-05-22 18:40:11 -0300

    specs

--- __tests__/Pokeform.spec.tsx
+++ __tests__/Pokeform.spec.tsx
@@ -1,2 +1,21 @@
-import { PokeformI } from "../src/utils/interfaces/forms.interface";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import Pokeform from "../src/components/organism/Pokeform";
+import store from "../src/redux/store";
 
+const Component = <Provider store={store}><Pokeform/></Provider>
+
+describe("Pokeform", () => {
+    describe("Validation", () => {
+        it("Name is required", async() => {
+            const form = render(Component);
+            const name = await form.findByLabelText("Nombre:");
+            expect(name.attributes.getNamedItem("required")).toBeDefined();
+        })
+        it("Image is required", async() => {
+            const form = render(Component);
+            const image = await form.findByLabelText("Imágen:");
+            expect(image.attributes.getNamedItem("required")).toBeDefined();
+        })
+    })
+})
--- src/components/atom/Input/index.tsx
+++ src/components/atom/Input/index.tsx
@@ -2,18 +2,28 @@ import React, { useEffect, useRef } from "react";
 import styles from "./style.module.scss";
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
-    name: string;
-    label: string;
+  name: string;
+  label: string;
 }
 
 export default function Input({ name, label, ...props }: Props) {
-    return (
-        <div className={styles.field}>
-            <label htmlFor={name}>{label}</label>
-            <div id={styles.container}>
-                <input name={name} type="text" {...props} placeholder={props.placeholder}></input>
-                {props.required && <span id={styles.error}>This field is required</span>}
-            </div>
-        </div>
-    );
+  
+  return (
+    <div className={styles.field}>
+      <label id={label} htmlFor={name}>
+        {label}
+      </label>
+      <div id={styles.container}>
+        <input
+          id={name}
+          aria-labelledby={label}
+          name={name}
+          type="text"
+          {...props}
+          placeholder={props.placeholder}
+        ></input>
+        {props.required && <span id={styles.error}>This field is required</span>}
+      </div>
+    </div>
+  );
 }
